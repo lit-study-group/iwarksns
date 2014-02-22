d8356e3bb4825bfd046ce87412b771ad143f2a32
Iwark <iwark02@gmail.com>
2014-02-22 22:45:06 +0900

    users assets and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    user.save
+    redirect_to root_url
+  end
+
+  private
+    #strong parameter
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+end
