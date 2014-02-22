3f89da7617be8f28b31629f825ce15f5f9123788
tuvistavie <tuvistavie@gmail.com>
2014-02-23 01:00:20 +0900

    Change locale to japanese. Add ja default locale file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,6 @@ class UsersController < ApplicationController
   private
     #strong parameter
     def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
+      params.require(:user).permit(:name, :password, :password_confirmation, :email)
     end
 end
