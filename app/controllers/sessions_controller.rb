5380fbd5166ad6d8f3f2deb3385237f29e263c14
tuvistavie <tuvistavie@gmail.com>
2014-02-23 19:22:31 +0900

    Fix authentication process to avoid double redirection and fill in login form properly.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,20 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user!, only:[:create]
+
   def create
-    user = User.find_by_email(params[:email])
-    session[:user_id] = user.id if user && user.authenticate(params[:password])
-    redirect_to root_url
+    user = User.find_by(email: params[:user][:email])
+               .try(:authenticate, params[:user][:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to posts_path
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url
+    redirect_to root_path
   end
 
 end
