19344d536d923822c0752d62a368298db0a3778e
tuvistavie <tuvistavie@gmail.com>
2014-02-23 19:13:23 +0900

    Rename methods. Remove useless dialog.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :check_logined, only:[:create]
+  skip_before_action :authenticate_user!, only:[:create]
   def create
     user = User.find_by_email(params[:email])
     session[:user_id] = user.id if user && user.authenticate(params[:password])
