8df23191adf5722a1bcc51af82a5ce70c931f738
Iwark <iwark02@gmail.com>
2014-02-23 16:25:05 +0900

    logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Iwarksns::Application.routes.draw do
   root 'welcome#index'
   resources :users, only: [:create, :show]
   resources :posts, only: [:index, :create, :destroy]
-  resources :sessions, only: [:create]
+  resources :sessions, only: [:create, :destroy]
 end
