41dcaa1967798102b9bcb84ce506418147865701
Iwark <iwark02@gmail.com>
2014-02-25 05:13:55 +0900

    edit welcome_controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,13 +3,12 @@ class WelcomeController < ApplicationController
   before_action :check_welcome_logined, only:[:index]
 
   def index
-    @user = User.new
     @new_user = User.new
   end
 
   private
 
   def check_welcome_logined
-    redirect_to current_user if current_user
+    redirect_to root_path if user_signed_in?
   end
 end
